8524c552c084d95a3e75978edf4bc7cf308a2802
hyejunn <84557643+hyejunn@users.noreply.github.com>
2022-07-13 19:36:57 +0900

    스코어, 속도 증가 등 기능 추가

--- tetris.cpp
+++ tetris.cpp
@@ -15,12 +15,21 @@
 #define RIGHT 77
 #define UP 72
 #define DOWN 80
+#define SPACE 32
+#define PAUSE1 112
+#define PAUSE2 80
+#define ESC 27
+
+#define SCREEN_X_ADJ 3
+#define SCREEN_Y_ADJ 1
+#define INFO_X_ADJ SCREEN_X_ADJ + WIDTH + 2
 
 int screen[HEIGHT][WIDTH];
 int screenCopy[HEIGHT][WIDTH];
 
 int bx, by;
 int blockType = 0;
+int nextBlockType = 0;
 int spin = 0;
 
 int blocks[7][4][4][4] =
@@ -42,21 +51,32 @@ int blocks[7][4][4][4] =
 };
 
 int key = 0;
+int score = 0;
+int speed = 100;
+int gameLevel = 0;
+int last_score = 0;
+int best_score = 0;
 bool crushFlag = false;
+bool spaceFlag = false;
+
 
 void CursorView();
 void gotoxy(short, short);
 void reset();
 void draw();
+void drawInfo();
 void newBlock();
 void downBlock();
 bool crush();
 void keyCheck();
-void blockMove(int move);
-bool canMove(int x, int y, int sp);
+void blockMove(int);
+bool canMove(int, int, int);
 void lineClear();
+void speedUp(int);
 void checkGameOver();
 void gameOver();
+void pause();
+
 
 int main() {
 
@@ -68,7 +88,11 @@ int main() {
 		for (int i = 0; i < 5; i++) {
 			keyCheck();
 			draw();
-			Sleep(100);
+			Sleep(speed);
+			if (spaceFlag) {
+				spaceFlag = false;
+				break;
+			}
 		}
 		downBlock();
 		lineClear();
@@ -97,6 +121,21 @@ void gotoxy(short x, short y) {
 
 
 void reset() {
+	FILE* file;
+	fopen_s(&file, "score.dat", "rt");
+	if (file == 0) best_score = 0;
+	else {
+		fscanf_s(file, "%d", &best_score);
+		fclose(file);
+	}
+	
+
+	key = 0;
+	crushFlag = false;
+	score = 0;
+	speed = 100;
+	nextBlockType = rand() % 7;
+
 	system("cls");
 	for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++) {
@@ -113,6 +152,7 @@ void reset() {
 	}
 
 	draw();
+	drawInfo();
 	newBlock();
 }
 
@@ -120,7 +160,7 @@ void draw() {
 	for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++) {
 			if (screen[i][j] != screenCopy[i][j]) {
-				gotoxy(j, i);
+				gotoxy(SCREEN_X_ADJ + j, SCREEN_Y_ADJ + i);
 				switch (screen[i][j]) {
 				case EMPTY:
 					printf("  ");
@@ -141,11 +181,35 @@ void draw() {
 }
 
 
+void drawInfo() {
+	int y = SCREEN_Y_ADJ + 5;
+	
+	gotoxy(INFO_X_ADJ, y + 1); printf("+-- N E X T --+");
+	gotoxy(INFO_X_ADJ, y + 2); printf("|             |");
+	gotoxy(INFO_X_ADJ, y + 3); printf("|             |");
+	gotoxy(INFO_X_ADJ, y + 4); printf("|             |");
+	gotoxy(INFO_X_ADJ, y + 5); printf("|             |");
+	gotoxy(INFO_X_ADJ, y + 6); printf("+-------------+");
+
+	gotoxy(INFO_X_ADJ, y + 8); printf(" YOUR SCORE :");
+	gotoxy(INFO_X_ADJ, y + 9); printf("        %6d", score);
+	gotoxy(INFO_X_ADJ, y + 10); printf(" LAST SCORE :");
+	gotoxy(INFO_X_ADJ, y + 11); printf("        %6d", last_score);
+	gotoxy(INFO_X_ADJ, y + 12); printf(" BEST SCORE :");
+	gotoxy(INFO_X_ADJ, y + 13); printf("        %6d", best_score);
+	gotoxy(INFO_X_ADJ, y + 15); printf("  ¡â   : Shift        SPACE : Hard Drop");
+	gotoxy(INFO_X_ADJ, y + 16); printf("¢·  ¢¹ : Left / Right   P   : Pause");
+	gotoxy(INFO_X_ADJ, y + 17); printf("  ¡ä   : Soft Drop     ESC  : Quit");
+
+}
+
+
 void newBlock() {
 	bx = (WIDTH / 2) - 1;
 	by = 0;
-	blockType = rand() % 7;
+	blockType = nextBlockType;
 	crushFlag = false;
+	nextBlockType = rand() % 7;
 
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
@@ -153,6 +217,17 @@ void newBlock() {
 				screen[by + i][bx + j] = ACTIVE_BLOCK;
 		}
 	}
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
+			gotoxy(INFO_X_ADJ + 2 + j, 9 + i);
+			if (blocks[nextBlockType][0][i][j] == 1)
+				printf("¡à");
+			else
+				printf("  ");
+		}
+	}
+
 
 }
 
@@ -202,11 +277,46 @@ void keyCheck() {
 			case UP:
 				if (canMove(bx, by, (spin + 1) % 4))
 					blockMove(UP);
+				else if (canMove(bx + 1, by, (spin + 1) % 4)) {
+					blockMove(RIGHT);
+					blockMove(UP);
+				}
+				else if (canMove(bx - 1, by, (spin + 1) % 4)) {
+					blockMove(LEFT);
+					blockMove(UP);
+				}
+				else if (blockType == 0 && canMove(bx - 2, by, (spin + 1) % 4) && bx == 8) {
+					blockMove(LEFT);
+					blockMove(LEFT);
+					blockMove(UP);
+				}
 				break;
 			case DOWN:
 				if (canMove(bx, by + 1, spin))
 					blockMove(DOWN);
 				break;
+			}
+		}
+		else {
+			int cnt = 0;
+			switch (key) {
+			case SPACE:
+				spaceFlag = true;
+				
+				while (!crush()) {
+					downBlock();
+					cnt++;
+				}
+				score += cnt / 3 * (gameLevel / 2 + 1);
+				gotoxy(INFO_X_ADJ, SCREEN_Y_ADJ + 14); printf("        %6d", score);
+				break;
+			case PAUSE1:
+			case PAUSE2:
+				pause();
+				break;
+			case ESC:
+				system("cls");
+				exit(0);
 			}
 		}
 	}
@@ -290,6 +400,7 @@ bool canMove(int x, int y, int sp) {
 
 
 void lineClear() {
+	int combo = 0;
 	bool clear = true;
 	for (int i = HEIGHT - 2; i >= 0; i--) {
 		for (int j = 1; j < WIDTH - 1; j++) {
@@ -299,15 +410,54 @@ void lineClear() {
 			}
 		}
 		if (clear) {
+			combo++;
 			for (int k = i; k > 0; k--) {
 				for (int l = 1; l < WIDTH - 1; l++) {
 					screen[k][l] = screen[k - 1][l];
 				}
 			}
 			i++;
+			score += 100 * (gameLevel * 2 + 1);
+			gotoxy(INFO_X_ADJ, SCREEN_Y_ADJ + 14); printf("        %6d", score);
+			if (score >= 20000) speedUp(6);
+			else if (score >= 15000) speedUp(5);
+			else if (score >= 10000) speedUp(4);
+			else if (score >= 5000) speedUp(3);
+			else if (score >= 2000) speedUp(2);
+			else if (score >= 1000) speedUp(1);
 		}
 		clear = true;
 	}
+	if (combo > 1) {
+		gotoxy(SCREEN_X_ADJ + (WIDTH / 2) - 1, SCREEN_Y_ADJ + by - 2); printf("%d COMBO!", combo);
+		Sleep(500);
+		score += (combo * 100 * (gameLevel + 1));
+	}
+}
+
+
+void speedUp(int level) {
+	switch (level) {
+	case 1:
+		speed = 50;
+		break;
+	case 2:
+		speed = 30;
+		break;
+	case 3:
+		speed = 20;
+		break;
+	case 4:
+		speed = 10;
+		break;
+	case 5:
+		speed = 8;
+		break;
+	case 6:
+		speed = 5;
+		break;
+	}
+	gameLevel = level;
 }
 
 
@@ -322,8 +472,41 @@ void checkGameOver() {
 
 
 void gameOver() {
-	gotoxy(WIDTH + 5, HEIGHT / 2); printf("G A M E  O V E R..");
+	int x = 5;
+	int y = 5;
+	gotoxy(x, y + 0); printf("¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç");
+	gotoxy(x, y + 1); printf("¢Ç                              ¢Ç");
+	gotoxy(x, y + 2); printf("¢Ç  +-----------------------+   ¢Ç");
+	gotoxy(x, y + 3); printf("¢Ç  |  G A M E  O V E R..   |   ¢Ç");
+	gotoxy(x, y + 4); printf("¢Ç  +-----------------------+   ¢Ç");
+	gotoxy(x, y + 5); printf("¢Ç   YOUR SCORE: %6d         ¢Ç", score);
+	gotoxy(x, y + 6); printf("¢Ç                              ¢Ç");
+	gotoxy(x, y + 7); printf("¢Ç  Press any key to restart..  ¢Ç");
+	gotoxy(x, y + 8); printf("¢Ç                              ¢Ç");
+	gotoxy(x, y + 9); printf("¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç¢Ç");
+	last_score = score;
+
+	if (score > best_score) {
+		FILE* file;
+		fopen_s(&file, "score.dat", "wt");
+
+		gotoxy(x, y + 6); printf("¢Ç  ¡Ú¡Ú¡Ú BEST SCORE! ¡Ú¡Ú¡Ú   ¢Ç  ");
+
+		if (file == 0) {
+			gotoxy(0, 0);
+			printf("FILE ERROR: SYSTEM CANNOT WRITE BEST SCORE ON \"SCORE.DAT\"");
+		}
+		else {
+			fprintf(file, "%d", score);
+			fclose(file);
+		}
+	}
 	Sleep(1000);
 	while (_kbhit()) key = _getch();
 	key = _getch();
+}
+
+
+void pause() {
+
 }
